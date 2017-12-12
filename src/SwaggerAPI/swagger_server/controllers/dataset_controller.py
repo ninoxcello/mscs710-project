4ab3d35975fa324310a595de3d31d19cde1af76c
Matt Maffa <maffadev@gmail.com>
2017-12-12 13:07:55 -0500

    Updated Models to match up with the demo jupyter notebook file
    Pretty much a complete overhaul of the dataset controller

--- src/SwaggerAPI/swagger_server/controllers/dataset_controller.py
+++ src/SwaggerAPI/swagger_server/controllers/dataset_controller.py
@@ -1,94 +1,22 @@
 import connexion
 import six
 
+from swagger_server.models.api_response import ApiResponse  # noqa: E501
 from swagger_server.models.inline_image import InlineImage  # noqa: E501
 from swagger_server.models.inline_response200 import InlineResponse200  # noqa: E501
-from swagger_server.models.inline_response2001 import InlineResponse2001  # noqa: E501
 from swagger_server.models.table import Table  # noqa: E501
 from swagger_server import util
 
 
-def dataset_get_candlestick_graph_currency_name_post(currencyName):  # noqa: E501
-    """Returns a candlestick graph of the specified currency
+def dataset_currency_selection_currency_name_post(currencyName):  # noqa: E501
+    """Selects a specific currency type
 
-    Sends data about what the graph should show and returns with an image of the graph # noqa: E501
+    The currency type should be either bitcoin, bitconnect, dash, ethereum, iota, litecoin, monero, nem, neo, numeraire, omisego, qtum, ripple, stratis, or waves # noqa: E501
 
-    :param currencyName: 
+    :param currencyName: Should be equal to either bitcoin, bitconnect, dash, ethereum, iota, litecoin, monero, nem, neo, numeraire, omisego, qtum, ripple, stratis, or waves
     :type currencyName: str
 
-    :rtype: InlineImage
-    """
-    return 'do some magic!'
-
-
-def dataset_get_cryptocurrency_correlation_graph_get():  # noqa: E501
-    """Send specification about what data to graph
-
-    Sends data about what the graph should show and returns with an image of the graph # noqa: E501
-
-
-    :rtype: InlineImage
-    """
-    return 'do some magic!'
-
-
-def dataset_get_graph_close_all_time_get():  # noqa: E501
-    """Returns a graph of all currencies closing values for all days we have data of
-
-    Sends data about what the graph should show and returns with an image of the graph # noqa: E501
-
-
-    :rtype: InlineImage
-    """
-    return 'do some magic!'
-
-
-def dataset_get_graph_close_one_year_get():  # noqa: E501
-    """Returns a graph of all currencies closing values for one year
-
-    Sends data about what the graph should show and returns with an image of the graph # noqa: E501
-
-
-    :rtype: InlineImage
-    """
-    return 'do some magic!'
-
-
-def dataset_get_graph_graphdata_post(graphdata):  # noqa: E501
-    """Unsupported Method
-
-    Sends data about what the graph should show and returns with an image of the graph # noqa: E501
-
-    :param graphdata: 
-    :type graphdata: str
-
-    :rtype: InlineResponse200
-    """
-    return 'do some magic!'
-
-
-def dataset_get_graph_open_close_currency_name_post(currencyName):  # noqa: E501
-    """Produces a graph with a specific currency&#39;s opening and closing values
-
-    Sends data about what the graph should show and returns with an image of the graph # noqa: E501
-
-    :param currencyName: 
-    :type currencyName: str
-
-    :rtype: InlineImage
-    """
-    return 'do some magic!'
-
-
-def dataset_get_graph_open_close_high_low_currency_name_post(currencyName):  # noqa: E501
-    """Produces a graph with a specific currency&#39;s opening, closing, high, and low values.
-
-    Sends data about what the graph should show and returns with an image of the graph # noqa: E501
-
-    :param currencyName: 
-    :type currencyName: str
-
-    :rtype: InlineImage
+    :rtype: ApiResponse
     """
     return 'do some magic!'
 
@@ -99,7 +27,7 @@ def dataset_get_stats_get():  # noqa: E501
     Returns values of count, mean, median, mode, min, max, standard deviation (std) for the dataset # noqa: E501
 
 
-    :rtype: InlineResponse2001
+    :rtype: InlineResponse200
     """
     return 'do some magic!'
 
@@ -113,5 +41,70 @@ def dataset_get_table_table_name_post(tableName):  # noqa: E501
     :type tableName: str
 
     :rtype: Table
+    """
+    return 'do some magic!'
+
+
+def dataset_graph_correlation_correlation_type_post(correlationType):  # noqa: E501
+    """Returns a graph utilizing the selected correlation type
+
+    1. Spearman, 2. Pearson, 3. Kendall # noqa: E501
+
+    :param correlationType: information
+    :type correlationType: str
+
+    :rtype: InlineImage
+    """
+    return 'do some magic!'
+
+
+def dataset_graph_selection_graph_selection_post(graphSelection):  # noqa: E501
+    """Selects a graph type
+
+    1. Trend Curve, 2. Candlestick, 3. Correlation Map # noqa: E501
+
+    :param graphSelection: Must be a value equal to 1, 2, or 3
+    :type graphSelection: str
+
+    :rtype: ApiResponse
+    """
+    return 'do some magic!'
+
+
+def dataset_model_type_model_type_post(modelType):  # noqa: E501
+    """Selects a model type
+
+    1. Linear Regression, 2. Support Vector Regression, 3. Multilayer Perceptron # noqa: E501
+
+    :param modelType: Should be a number equal to 1, 2, or 3
+    :type modelType: int
+
+    :rtype: ApiResponse
+    """
+    return 'do some magic!'
+
+
+def dataset_operation_type_operation_type_post(operationType):  # noqa: E501
+    """Selects what operation to do, either training, testing, or prediction
+
+    1. Train, 2. Test, 3. Predict # noqa: E501
+
+    :param operationType: information
+    :type operationType: str
+
+    :rtype: ApiResponse
+    """
+    return 'do some magic!'
+
+
+def dataset_train_currency_name_post(currencyName):  # noqa: E501
+    """Trains based on the currency selected
+
+    Should be either bitcoin, bitconnect, dash, ethereum, iota, litecoin, monero, nem, neo, numeraire, omisego, qtum, ripple, stratis, or waves # noqa: E501
+
+    :param currencyName: Should be equal to either bitcoin, bitconnect, dash, ethereum, iota, litecoin, monero, nem, neo, numeraire, omisego, qtum, ripple, stratis, or waves
+    :type currencyName: str
+
+    :rtype: ApiResponse
     """
     return 'do some magic!'
--- src/SwaggerAPI/swagger_server/models/inline_response200.py
+++ src/SwaggerAPI/swagger_server/models/inline_response200.py
@@ -15,21 +15,56 @@ class InlineResponse200(Model):
     Do not edit the class manually.
     """
 
-    def __init__(self, imageurl: str=None):  # noqa: E501
+    def __init__(self, count: float=None, mean: float=None, std: float=None, min: float=None, _25: float=None, _50: float=None, _75: float=None, max: float=None):  # noqa: E501
         """InlineResponse200 - a model defined in Swagger
 
-        :param imageurl: The imageurl of this InlineResponse200.  # noqa: E501
-        :type imageurl: str
+        :param count: The count of this InlineResponse200.  # noqa: E501
+        :type count: float
+        :param mean: The mean of this InlineResponse200.  # noqa: E501
+        :type mean: float
+        :param std: The std of this InlineResponse200.  # noqa: E501
+        :type std: float
+        :param min: The min of this InlineResponse200.  # noqa: E501
+        :type min: float
+        :param _25: The _25 of this InlineResponse200.  # noqa: E501
+        :type _25: float
+        :param _50: The _50 of this InlineResponse200.  # noqa: E501
+        :type _50: float
+        :param _75: The _75 of this InlineResponse200.  # noqa: E501
+        :type _75: float
+        :param max: The max of this InlineResponse200.  # noqa: E501
+        :type max: float
         """
         self.swagger_types = {
-            'imageurl': str
+            'count': float,
+            'mean': float,
+            'std': float,
+            'min': float,
+            '_25': float,
+            '_50': float,
+            '_75': float,
+            'max': float
         }
 
         self.attribute_map = {
-            'imageurl': 'imageurl'
+            'count': 'count',
+            'mean': 'mean',
+            'std': 'std',
+            'min': 'min',
+            '_25': '25%',
+            '_50': '50%',
+            '_75': '75%',
+            'max': 'max'
         }
 
-        self._imageurl = imageurl
+        self._count = count
+        self._mean = mean
+        self._std = std
+        self._min = min
+        self.__25 = _25
+        self.__50 = _50
+        self.__75 = _75
+        self._max = max
 
     @classmethod
     def from_dict(cls, dikt) -> 'InlineResponse200':
@@ -43,22 +78,169 @@ class InlineResponse200(Model):
         return util.deserialize_model(dikt, cls)
 
     @property
-    def imageurl(self) -> str:
-        """Gets the imageurl of this InlineResponse200.
+    def count(self) -> float:
+        """Gets the count of this InlineResponse200.
 
 
-        :return: The imageurl of this InlineResponse200.
-        :rtype: str
+        :return: The count of this InlineResponse200.
+        :rtype: float
         """
-        return self._imageurl
+        return self._count
 
-    @imageurl.setter
-    def imageurl(self, imageurl: str):
-        """Sets the imageurl of this InlineResponse200.
+    @count.setter
+    def count(self, count: float):
+        """Sets the count of this InlineResponse200.
 
 
-        :param imageurl: The imageurl of this InlineResponse200.
-        :type imageurl: str
+        :param count: The count of this InlineResponse200.
+        :type count: float
         """
 
-        self._imageurl = imageurl
+        self._count = count
+
+    @property
+    def mean(self) -> float:
+        """Gets the mean of this InlineResponse200.
+
+
+        :return: The mean of this InlineResponse200.
+        :rtype: float
+        """
+        return self._mean
+
+    @mean.setter
+    def mean(self, mean: float):
+        """Sets the mean of this InlineResponse200.
+
+
+        :param mean: The mean of this InlineResponse200.
+        :type mean: float
+        """
+
+        self._mean = mean
+
+    @property
+    def std(self) -> float:
+        """Gets the std of this InlineResponse200.
+
+
+        :return: The std of this InlineResponse200.
+        :rtype: float
+        """
+        return self._std
+
+    @std.setter
+    def std(self, std: float):
+        """Sets the std of this InlineResponse200.
+
+
+        :param std: The std of this InlineResponse200.
+        :type std: float
+        """
+
+        self._std = std
+
+    @property
+    def min(self) -> float:
+        """Gets the min of this InlineResponse200.
+
+
+        :return: The min of this InlineResponse200.
+        :rtype: float
+        """
+        return self._min
+
+    @min.setter
+    def min(self, min: float):
+        """Sets the min of this InlineResponse200.
+
+
+        :param min: The min of this InlineResponse200.
+        :type min: float
+        """
+
+        self._min = min
+
+    @property
+    def _25(self) -> float:
+        """Gets the _25 of this InlineResponse200.
+
+
+        :return: The _25 of this InlineResponse200.
+        :rtype: float
+        """
+        return self.__25
+
+    @_25.setter
+    def _25(self, _25: float):
+        """Sets the _25 of this InlineResponse200.
+
+
+        :param _25: The _25 of this InlineResponse200.
+        :type _25: float
+        """
+
+        self.__25 = _25
+
+    @property
+    def _50(self) -> float:
+        """Gets the _50 of this InlineResponse200.
+
+
+        :return: The _50 of this InlineResponse200.
+        :rtype: float
+        """
+        return self.__50
+
+    @_50.setter
+    def _50(self, _50: float):
+        """Sets the _50 of this InlineResponse200.
+
+
+        :param _50: The _50 of this InlineResponse200.
+        :type _50: float
+        """
+
+        self.__50 = _50
+
+    @property
+    def _75(self) -> float:
+        """Gets the _75 of this InlineResponse200.
+
+
+        :return: The _75 of this InlineResponse200.
+        :rtype: float
+        """
+        return self.__75
+
+    @_75.setter
+    def _75(self, _75: float):
+        """Sets the _75 of this InlineResponse200.
+
+
+        :param _75: The _75 of this InlineResponse200.
+        :type _75: float
+        """
+
+        self.__75 = _75
+
+    @property
+    def max(self) -> float:
+        """Gets the max of this InlineResponse200.
+
+
+        :return: The max of this InlineResponse200.
+        :rtype: float
+        """
+        return self._max
+
+    @max.setter
+    def max(self, max: float):
+        """Sets the max of this InlineResponse200.
+
+
+        :param max: The max of this InlineResponse200.
+        :type max: float
+        """
+
+        self._max = max
