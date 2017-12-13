31859ef10e82a2212190f6925f903cc161bc0e07
Matt Maffa <maffadev@gmail.com>
2017-12-13 17:22:23 -0500

    Added code to dataset controller
    Still work in progress due to code being moved from ordered execution in jupyter notebook.
    
    Also added python init files to signify that directories contain python files

--- src/SwaggerAPI/swagger_server/controllers/dataset_controller.py
+++ src/SwaggerAPI/swagger_server/controllers/dataset_controller.py
@@ -7,6 +7,44 @@ from swagger_server.models.inline_response200 import InlineResponse200  # noqa: 
 from swagger_server.models.table import Table  # noqa: E501
 from swagger_server import util
 
+import sys
+import os
+
+sys.path.append(os.path.abspath("C:/Users/Matt/Documents/GitHub/mscs710-project/src"))
+from utils import *
+
+import datetime
+import math
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+import warnings
+from matplotlib import style
+from subprocess import check_output
+
+import models
+import utils
+import visuals
+
+warnings.filterwarnings("ignore")
+style.use('ggplot')
+plt.rcParams['figure.figsize'] = (12.0, 8.0)
+
+#Globals
+currencies = "UNDEFINED"
+coin_type = "UNDEFINED"
+coin_feat = "UNDEFINED"
+model = "UNDEFINED"
+x_train = "UNDEFINED"
+y_train = "UNDEFINED"
+x_test = "UNDEFINED"
+y_test = "UNDEFINED"
+x_recent = "UNDEFINED"
+name = "UNDEFINED"
+input_dir = "UNDEFINED"
+df = utils.load_data(input_dir, name)
 
 def dataset_currency_selection_currency_name_post(currencyName):  # noqa: E501
     """Selects a specific currency type
@@ -18,7 +56,31 @@ def dataset_currency_selection_currency_name_post(currencyName):  # noqa: E501
 
     :rtype: ApiResponse
     """
-    return 'do some magic!'
+    print('Enter Currency File Name: ')
+    # name = input()
+    global name
+    name = 'bitcoin_price.csv'
+
+    x_train, x_test, x_recent, y_train, y_test, df = utils.load_data(input_dir, name)
+
+    def print_summary(x_train, x_test, x_recent, y_train, y_test, df):
+        print('=================================================')
+        print('Date of newest data: {}'.format(df.index[0]))
+        print('Date of oldest data: {}\n'.format(df.index[-1]))
+
+        # Dataset shapes
+        print(x_train.shape[0], 'training samples.')
+        print(x_test.shape[0], 'test samples.')
+        print('Predicting {} days'.format(x_recent.shape[0]))
+        print('Train sample shape: ', x_train.shape)
+        print('Test sample shape: ', x_test.shape)
+        print('Train label shape:', y_train.shape)
+        print('Test label shape:', y_test.shape)
+        # sample data statistics
+        print('Sample Data: ')
+        print(df.describe())
+
+    return print_summary(x_train, x_test, x_recent, y_train, y_test, df)
 
 
 def dataset_get_stats_get():  # noqa: E501
@@ -55,7 +117,23 @@ def dataset_graph_correlation_correlation_type_post(correlationType):  # noqa: E
 
     :rtype: InlineImage
     """
-    return 'do some magic!'
+    #print('Choose Graph Type: [1]Trend Curve [2]Candlestick [3]Correlation Map')
+    global choice
+    choice = correlationType
+    if choice == 1:
+        visuals.plot_trend(currencies, coin_type, coin_feat)
+    elif choice == 2:
+        visuals.plot_candlestick(currencies, coin_type, coin_feat)
+    elif choice == 3:
+        print('Choose correlation type: [1]Spearman [2]Pearson [3]Kendall')
+        corr_choice = int(input())
+        if corr_choice == 1:
+            return visuals.plot_correlation('spearman')
+        elif corr_choice == 2:
+            return visuals.plot_correlation('pearson')
+        elif corr_choice == 3:
+            return visuals.plot_correlation('kendall')
+    return 'Error'
 
 
 def dataset_graph_selection_graph_selection_post(graphSelection):  # noqa: E501
@@ -68,19 +146,49 @@ def dataset_graph_selection_graph_selection_post(graphSelection):  # noqa: E501
 
     :rtype: ApiResponse
     """
-    return 'do some magic!'
+    global choice
+    choice = graphSelection
+    if choice == 1:
+        visuals.plot_trend(currencies, coin_type, coin_feat)
+    elif choice == 2:
+        visuals.plot_candlestick(currencies, coin_type, coin_feat)
+    elif choice == 3:
+        print('Choose correlation type: [1]Spearman [2]Pearson [3]Kendall')
+        corr_choice = int(input())
+        if corr_choice == 1:
+            return visuals.plot_correlation('spearman')
+        elif corr_choice == 2:
+            return visuals.plot_correlation('pearson')
+        elif corr_choice == 3:
+            return visuals.plot_correlation('kendall')
+    return 'Error'
 
 
 def dataset_model_type_model_type_post(modelType):  # noqa: E501
     """Selects a model type
 
-    1. Linear Regression, 2. Support Vector Regression, 3. Multilayer Perceptron # noqa: E501
+    1. Linear Regression, 2. Support Vector Regression, 3. Multilayer Perceptron, 4. Gradient Boosting Regression # noqa: E501
 
-    :param modelType: Must be a number equal to 1, 2, or 3
+    :param modelType: Must be a number equal to 1, 2, 3, or 4
     :type modelType: int
 
     :rtype: ApiResponse
     """
+    choice = modelType
+
+    if choice == 1:
+        model = models.LR(x_train, y_train, x_test, y_test, x_recent)
+        print('Linear Regression model selected.\n')
+    elif choice == 2:
+        model = models.SVR(x_train, y_train, x_test, y_test, x_recent)
+        print('Support Vector Regression model selected.\n')
+    elif choice == 3:
+        model = models.MLP(x_train, y_train, x_test, y_test, x_recent)
+        model.build()
+        print('Multilayer Perceptron model selected.\n')
+    elif choice == 4:
+        model = models.GTBR(x_train, y_train, x_test, y_test, x_recent)
+        print('Gradient Boosting Regression model selected.\n')
     return 'do some magic!'
 
 
@@ -94,7 +202,20 @@ def dataset_operation_type_operation_type_post(operationType):  # noqa: E501
 
     :rtype: ApiResponse
     """
-    return 'do some magic!'
+    op = operationType
+
+    if op == 1:
+        print('Training initiated...\n')
+        model.train()
+    elif op == 2:
+        print('Evaluating model on test data...\n')
+        model.test()
+    elif op == 3:
+        print('Predicting future values...\n')
+        preds = model.predict()
+        print('Forecast Plot')
+        return utils.forecast_plot(df, preds)
+    return 'Error'
 
 
 def dataset_train_currency_name_post(currencyName):  # noqa: E501
@@ -107,4 +228,17 @@ def dataset_train_currency_name_post(currencyName):  # noqa: E501
 
     :rtype: ApiResponse
     """
+    print('Enter Currency File Name: ')
+    # name = input()
+    name = 'bitcoin_price.csv'
+
+    x_train, x_test, x_recent, y_train, y_test, df = utils.load_data(input_dir, name)
+    print('---------------------------------------')
+    print(x_train.shape[0], 'training samples.')
+    print(x_test.shape[0], 'test samples.')
+    print('Predicting {} days'.format(x_recent.shape[0]))
+    print('Train sample shape: ', x_train.shape)
+    print('Test sample shape: ', x_test.shape)
+    print('Train label shape:', y_train.shape)
+    print('Test label shape:', y_test.shape)
     return 'do some magic!'
